09a1f83935cdb595afe56b4c1accf89b24c9b67f
Luay Younus <luayghazwan@yahoo.com>
2017-11-22 17:38:11 -0800

    Added Controllers for Movies and Customers

---
+++ PacificStore/Controllers/CustomersController.cs
@@ -0,0 +1,38 @@
+﻿using PacificStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PacificStore.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public ViewResult Index()
+        {
+            var customers = GetCustomers();
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(customer);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer { Id = 1, Name = "Tom" },
+                new Customer { Id = 2, Name = "Bash" }
+            };
+        }
+    }
+}
---
+++ PacificStore/Controllers/MoviesController.cs
@@ -0,0 +1,27 @@
+﻿using PacificStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PacificStore.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek"},
+                new Movie { Id = 2, Name = "Wall-E"}
+            };
+        }
+    }
+}
