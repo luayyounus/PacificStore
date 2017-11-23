b6b5b76706c2597dbb102e4fa964a559a238bddf
Luay Younus <luayghazwan@yahoo.com>
2017-11-23 03:14:07 -0800

    Add BirthDate to Customer

--- PacificStore/Models/Customer.cs
+++ PacificStore/Models/Customer.cs
@@ -12,6 +12,7 @@ namespace PacificStore.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+        public DateTime? BirthDate { get; set; }
         public bool IsSubscribedToNewsLetter { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
