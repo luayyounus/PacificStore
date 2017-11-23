bce20b8508395294a9cbfc90b81545809a42047d
Luay Younus <luayghazwan@yahoo.com>
2017-11-23 01:49:08 -0800

    Updated Customers Controller to use Database context to populate Data

--- PacificStore/Controllers/CustomersController.cs
+++ PacificStore/Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
 ﻿using PacificStore.Models;
+using System.Data.Entity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,30 +10,33 @@ namespace PacificStore.Controllers
 {
     public class CustomersController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
             if (customer == null)
             {
                 return HttpNotFound();
             }
 
             return View(customer);
-        }
-
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer { Id = 1, Name = "Tom" },
-                new Customer { Id = 2, Name = "Bash" }
-            };
         }
     }
 }
