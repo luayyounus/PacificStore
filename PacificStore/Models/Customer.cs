9d402bdcb7afe80b6eac08205e99dfbeaa2bed35
Luay Younus <luayghazwan@yahoo.com>
2017-11-22 19:05:13 -0800

    New Customer Model properties added - Added Migration/Update Databse

--- PacificStore/Models/Customer.cs
+++ PacificStore/Models/Customer.cs
@@ -9,6 +9,8 @@ namespace PacificStore.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-
+        public bool IsSubscribedToNewsLetter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
     }
 }
