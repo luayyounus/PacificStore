49d88a04bd37a034280b7388542016aa91521a98
Luay Younus <luayghazwan@yahoo.com>
2017-11-22 17:27:15 -0800

    Initial Commit

---
+++ PacificStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PacificStore.Startup))]
+namespace PacificStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
