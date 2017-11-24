5d1830c36f93488b49405de0b51a68182f39670f
Luay Younus <luayghazwan@yahoo.com>
2017-11-24 01:15:08 -0800

    Populate Genre with Data

--- PacificStore/Migrations/Configuration.cs
+++ PacificStore/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace PacificStore.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationsEnabled = false;
         }
 
         protected override void Seed(PacificStore.Models.ApplicationDbContext context)
