6bb8019e2c8b18721a1ce2a8725c3ef4124b2538
Luay Younus <luayghazwan@yahoo.com>
2017-11-24 15:46:42 -0800

    Add Dbset for Memebership types

--- PacificStore/Models/IdentityModels.cs
+++ PacificStore/Models/IdentityModels.cs
@@ -21,8 +21,8 @@ namespace PacificStore.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public DbSet<Customer> Customers { get; set; }
-
         public DbSet<Movie> Movies { get; set; }
+        public DbSet<MembershipType> MembershipTypes { get; set; }
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
