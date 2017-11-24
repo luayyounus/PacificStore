96964bad529dc8dca48332dac0966706f1e9696b
Luay Younus <luayghazwan@yahoo.com>
2017-11-24 15:48:09 -0800

    Add ViewModel for new customer

---
+++ PacificStore/ViewModels/NewCustomerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using PacificStore.Models;
+
+namespace PacificStore.ViewModels
+{
+    public class NewCustomerViewModel
+    {
+        public IEnumerable<MembershipType> MembershipTypes { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
