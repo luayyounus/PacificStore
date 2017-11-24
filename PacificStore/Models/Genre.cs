364601db719e64f2a0efb0daecc0875010be352f
Luay Younus <luayghazwan@yahoo.com>
2017-11-23 23:42:43 -0800

    Modified Movie Model and Created Genre Model

--- PacificStore/Models/Genre.cs
+++ PacificStore/Models/Genre.cs
@@ -6,12 +6,10 @@ using System.Web;
 
 namespace PacificStore.Models
 {
-    public class Movie
+    public class Genre
     {
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
-        [Required]
-        public Genre Genre { get; set; }
     }
 }
