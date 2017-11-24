17dd98116998d70276e19c297240f6a1595131a5
Luay Younus <luayghazwan@yahoo.com>
2017-11-23 23:43:16 -0800

    Add Migration for Genre

--- PacificStore/Migrations/Configuration.cs
+++ PacificStore/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace PacificStore.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(PacificStore.Models.ApplicationDbContext context)
