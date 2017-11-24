e3fa71f316a07c9d84e76ff59d90ef13bd6b1243
Luay Younus <luayghazwan@yahoo.com>
2017-11-24 01:18:01 -0800

    New Properties to Movie Model

--- PacificStore/Models/Movie.cs
+++ PacificStore/Models/Movie.cs
@@ -13,5 +13,11 @@ namespace PacificStore.Models
         public string Name { get; set; }
         [Required]
         public Genre Genre { get; set; }
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+        [Required]
+        public DateTime DateAdded { get; set; }
+        [Required]
+        public int NumberInStock { get; set; }
     }
 }
