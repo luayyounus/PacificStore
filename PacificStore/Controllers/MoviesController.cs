0819c43dcb3669cdaa09dccaa6482686195bc431
Luay Younus <luayghazwan@yahoo.com>
2017-11-24 02:09:33 -0800

    Add Action for Movie Details

--- PacificStore/Controllers/MoviesController.cs
+++ PacificStore/Controllers/MoviesController.cs
@@ -27,5 +27,15 @@ namespace PacificStore.Controllers
             var movies = _context.Movies.Include(m => m.Genre).ToList();
             return View(movies);
         }
+
+        public ActionResult Details(int id)
+        {
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        } 
     }
 }
