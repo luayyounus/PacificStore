a7251d24d74d8c5e89dda4a5f11d500dcbdaefdf
Luay Younus <luayghazwan@yahoo.com>
2017-11-25 22:09:21 -0800

    Form validation added to Customer

--- PacificStore/Controllers/CustomersController.cs
+++ PacificStore/Controllers/CustomersController.cs
@@ -36,6 +36,16 @@ namespace PacificStore.Controllers
         [HttpPost]
         public ActionResult Save(Customer customer)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+                return View("CustomerForm", viewModel);
+            }
+
             if (customer.Id == 0)
                 _context.Customers.Add(customer);
             else
