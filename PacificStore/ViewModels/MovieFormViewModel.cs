9c235306d6df1205eb2c1cc9b826de1ff820fe0d
Luay Younus <luayghazwan@yahoo.com>
2017-11-25 20:13:34 -0800

    Movie view model for Movie and its Genres

---
+++ PacificStore/ViewModels/MovieFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PacificStore.Models;
+
+namespace PacificStore.ViewModels
+{
+    public class MovieFormViewModel
+    {
+        public IEnumerable<Genre> Genres { get; set; }
+        public Movie Movie { get; set; }
+    }
+}
