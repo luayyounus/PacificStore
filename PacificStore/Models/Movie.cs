8642f28eb543a655c1d21144d198b90b28e3ceeb
Luay Younus <luayghazwan@yahoo.com>
2017-11-25 20:10:49 -0800

    Add Genre Model and new movie properties

--- PacificStore/Models/Movie.cs
+++ PacificStore/Models/Movie.cs
@@ -9,15 +9,24 @@ namespace PacificStore.Models
     public class Movie
     {
         public int Id { get; set; }
+
         [Required]
         public string Name { get; set; }
+
         [Required]
-        public Genre Genre { get; set; }
-        [Required]
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
+
         [Required]
         public DateTime DateAdded { get; set; }
+
         [Required]
+        [Display(Name = "Number in Stock")]
         public int NumberInStock { get; set; }
+
+        public Genre Genre { get; set; }
+
+        [Display(Name = "Genre")]
+        public byte GenreId { get; set; }
     }
 }
