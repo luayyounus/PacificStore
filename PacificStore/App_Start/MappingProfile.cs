41ebfbdabf6c5bcfcb74bf31df50a6e9f7958591
Luay Younus <luayghazwan@yahoo.com>
2017-11-27 15:28:16 -0800

    Installed AutoMapper to Map Customer model with CustomerDto

---
+++ PacificStore/App_Start/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using PacificStore.Dtos;
+using PacificStore.Models;
+
+namespace PacificStore.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
--- PacificStore/Global.asax.cs
+++ PacificStore/Global.asax.cs
@@ -6,6 +6,8 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using AutoMapper;
+using PacificStore.App_Start;
 
 namespace PacificStore
 {
@@ -13,6 +15,7 @@ namespace PacificStore
     {
         protected void Application_Start()
         {
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
             GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
