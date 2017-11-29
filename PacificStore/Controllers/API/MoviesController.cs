c2d6ddf69990cdb56808cdaeb0a01127ff1494de
Luay Younus <luayghazwan@yahoo.com>
2017-11-28 23:57:09 -0800

    Api actions update

---
+++ PacificStore/Controllers/API/MoviesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using PacificStore.Models;
+
+namespace PacificStore.Controllers.API
+{
+    public class MoviesController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET api/movies
+        public IEnumerable<Movie> GetMovies()
+        {
+            return _context.Movies.ToList();
+        }
+
+        // GET /api/movies/1
+        public Movie GetMovie(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+            if (movie == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            return movie;
+        }
+
+        // GET /api/movies
+        [HttpPost]
+        public Movie CreateMovie(Movie movie)
+        {
+            if(!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+
+            return movie;
+        }
+
+        // GET /api/movies/1
+        [HttpPut]
+        public Movie Update
+        
+    }
+}
