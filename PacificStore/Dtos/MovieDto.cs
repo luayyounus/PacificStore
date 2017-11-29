101606279cc644d817e73699bab3edce19e38826
Luay Younus <luayghazwan@yahoo.com>
2017-11-29 11:10:19 -0800

    Movie Dto Created

---
+++ PacificStore/Dtos/MovieDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using PacificStore.Models;
+
+namespace PacificStore.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required]
+        public DateTime DateAdded { get; set; }
+
+        [Required]
+        [Range(0, 20)]
+        public int NumberInStock { get; set; }
+
+        public byte GenreId { get; set; }
+    }
+}
